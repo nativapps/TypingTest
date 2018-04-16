3043f01fea838334cb0236db94e0791a06a85ea1
confixyu <tonyyucen@gmail.com>
2018-04-15 21:24:21 -0500

    save point

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
-  devise_for :participants
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :rooms
 end
