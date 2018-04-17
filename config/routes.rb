fbe06a98832729aabdfdfb7c5a1db349442c4d05
confixyu <tonyyucen@gmail.com>
2018-04-17 10:53:46 -0500

    save point of test session

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,5 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :rooms
+  get 'room_test/index'
 end
