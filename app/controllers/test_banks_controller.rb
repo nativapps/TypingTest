d6c04242ed075fa325c89071477b3e92a76c51af
confixyu <tonyyucen@gmail.com>
2018-04-18 10:09:38 -0500

    complete the thes bank crud

--- app/controllers/test_banks_controller.rb
+++ app/controllers/test_banks_controller.rb
@@ -1,5 +1,54 @@
 class TestBanksController < ApplicationController
-    def index
-        @test = TestBank.all
-    end
+    before_action :authenticate_user!
+
+	def index
+		@tests = TestBank.all
+	end
+
+	def show
+		@test = TestBank.find(params[:id])
+	end
+
+	def new
+		@test = TestBank.new
+	end
+
+	def create
+		@test = TestBank.new(test_params)
+  		if @test.save
+  			redirect_to @test
+  		else
+  			render :new
+  		end
+	end
+
+	def edit
+    	@test = TestBank.find(params[:id])
+  	end
+
+  	def update
+		@test = TestBank.find(params[:id])
+  		if @test.update(test_params)
+  			redirect_to test_banks_path
+  		else
+  			render :edit
+  		end
+	end
+
+	def destroy
+		@test = TestBank.find(params[:id])
+		@test.destroy
+
+		if @test.destroy
+			redirect_to test_banks_path
+		else
+    	redirect_to @test
+		end
+	end
+
+	private
+
+	def test_params
+		params.require(:test_bank).permit(:name, :type_test, :text_test)
+	end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_04_17_213250) do
+ActiveRecord::Schema.define(version: 2018_04_18_144045) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -55,7 +55,7 @@ ActiveRecord::Schema.define(version: 2018_04_17_213250) do
 
   create_table "test_banks", force: :cascade do |t|
     t.string "name"
-    t.string "type"
+    t.string "type_test"
     t.text "text_test"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
