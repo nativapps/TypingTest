54ac0e90c20088dfd85bafd7b8628afe820420f9
ConfixYu <tonyyucen@gmail.com>
2018-04-30 10:07:10 -0500

    hot fix, limit session, and another

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_04_23_193256) do
+ActiveRecord::Schema.define(version: 2018_04_27_154132) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -60,6 +60,18 @@ ActiveRecord::Schema.define(version: 2018_04_23_193256) do
     t.integer "identification"
     t.index ["email"], name: "index_participants_on_email", unique: true
     t.index ["reset_password_token"], name: "index_participants_on_reset_password_token", unique: true
+  end
+
+  create_table "reports", force: :cascade do |t|
+    t.integer "try_number"
+    t.integer "correct"
+    t.integer "incorrect"
+    t.integer "total_type"
+    t.integer "total_word"
+    t.integer "wpm"
+    t.integer "acurrancy"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "rooms", force: :cascade do |t|
