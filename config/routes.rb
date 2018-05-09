b6e849a8802fd15fcecbbca08cd3add11cd0d743
Tony Yu <tyu@nativapps.co>
2018-05-09 07:49:54 -0500

    Almost finish, need to add the deliver email for the user choose their password

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
-  devise_for :participants
+
+  as :participant do
+    patch '/participant/confirmation' => 'confirmations#update', :via => :put, :as => :update_participant_confirmation
+  end
+  devise_for :participants, :controllers => { :confirmations => "confirmations" }
+
   resources :participants
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
