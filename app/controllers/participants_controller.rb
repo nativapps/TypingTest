58fc5dd0e45776fc29f84bce688e747c6e04e9db
Tony Yu <tyu@nativapps.co>
2018-05-25 11:27:16 -0500

    routing confirmation url to password form

--- app/controllers/participants_controller.rb
+++ app/controllers/participants_controller.rb
@@ -46,18 +46,6 @@ class ParticipantsController < ApplicationController
 		end
 	end
 
-	def update_password
-	  @user = current_user
-	  if @user.update(participant_params)
-	    # Sign in the user by passing validation in case their password changed
-	    bypass_sign_in(@user)
-	    redirect_to root_path
-	  else
-	    render "edit"
-	  end
-	end
-
-	
 	private
 
 	def participant_params
