fce90f40cc1082b9d3caedaf29011b5a8846f576
Tony Yu <tyu@nativapps.co>
2018-05-28 15:02:55 -0500

    Redirecting after confirm password

--- app/controllers/participants_controller.rb
+++ app/controllers/participants_controller.rb
@@ -29,7 +29,10 @@ class ParticipantsController < ApplicationController
 	def update
 		@participant = Participant.find(params[:id])
 		@participant.confirm if !@participant.confirmed?  
-		if @participant.update(participant_params)
+		if @participant.update(participant_params) && !@participant.confirmed?
+			@participant.confirm
+			redirect_to root_path
+		elsif @participant.update(participant_params)
 			redirect_to participants_path
 		else
 			render :edit
