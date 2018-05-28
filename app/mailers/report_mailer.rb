7939026a2cf7f1385d5b7707b66be38c9bbc2408
Tony Yu <jesscrdng@gmail.com>
2018-05-28 15:49:14 -0500

    Dummy commit

--- app/mailers/report_mailer.rb
+++ app/mailers/report_mailer.rb
@@ -1,2 +1,5 @@
-class ReportMailer < ApplicationMailer
+class ReportMailer < ApplicationMailer 
+  def send_report(report)
+    mail(to: participant_find_by_id(report.participant_id).email, subject: 'Typing Test Report')
+  end
 end
