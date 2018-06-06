65db5f3564e0e64dc464192a3a4a2fa22f73a403
Tony Yu <tyu@nativapps.co>
2018-06-05 20:41:17 -0500

    save

--- app/controllers/room_test_controller.rb
+++ app/controllers/room_test_controller.rb
@@ -1,52 +1,47 @@
 class RoomTestController < ApplicationController
   before_action :authenticate_participant!, :except => [:index]
 
-	def index
-		# This def is only for show the main page
-	end
+  def index
+    # This def is only for show the main page
+  end
 
-	def lobby
-		if current_participant.rooms 
-			@lobby_rooms = current_participant.rooms
-			render
-		else
-			@hola = 'nada paso!'
-		end
-	end
+  def lobby
+    if current_participant.rooms 
+      @lobby_rooms = current_participant.rooms
+      render
+    else
+      @hola = 'nada paso!'
+    end
+  end
 
-	def rooms
-		if current_participant.rooms.find(room_id).start_date <= Date.today && current_participant.rooms.find(room_id).finish_date >= Date.today
-			@tests = current_participant.rooms.find(room_id).test_banks
-			@room_id = room_id
-			@mensaje= current_participant.rooms.find(room_id).start_date
-		else
-			@mensaje = 'no era la fecha permitida'
-			@tests = []
-		end
-	end
+  def rooms
+    if current_participant.rooms.find(room_id).start_date <= Date.today && current_participant.rooms.find(room_id).finish_date >= Date.today
+      @tests = current_participant.rooms.find(room_id).test_banks
+      @room_id = room_id
+      @mensaje= current_participant.rooms.find(room_id).start_date
+    else
+      @mensaje = 'no era la fecha permitida'
+      @tests = []
+    end
+  end
 
   def test
     if current_participant
-			@time = current_participant.rooms.find(room_id).set_limit
+      @time = "Hola " # current_participant.rooms.find(room_id).tried_times
       @test = TestBank.find(params[:test_id])
-      if current_participant
-        @hola = current_participant
-        render
-      else
-        @hola = 'nada paso!'
-      end
+      render
     else
       redirect_to root_path
     end
-	end
-	
-	private
+  end
+  
+  private
 
-	def room_id
-		return params[:room_id]
-	end
+  def room_id
+    return params[:room_id]
+  end
 
-	def test_id
-		return params[:test_id]
-	end
+  def test_id
+    return params[:test_id]
+  end
 end
