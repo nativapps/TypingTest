ca1e8f52b76369851b42bd63d2748a1a3c388479
Tony Yu <tyu@nativapps.co>
2018-07-03 09:22:32 -0500

    Fixed the tried times deleting from room is not added in test

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -67,7 +67,7 @@ class RoomsController < ApplicationController
   private
 
   def room_params
-    params.require(:room).permit(:name, :start_date, :finish_date, :tried_times, :set_limit, :participants, :test_banks)
+    params.require(:room).permit(:name, :start_date, :finish_date, :set_limit, :participants, :test_banks)
   end
 
   def room_id
--- db/migrate/20180606014740_fix_try_time_room_and_test.rb
+++ db/migrate/20180606014740_fix_try_time_room_and_test.rb
@@ -1,6 +1,5 @@
 class FixTryTimeRoomAndTest < ActiveRecord::Migration[5.2]
   def change
-    remove_column :rooms, :tried_times
     add_column :test_banks, :try_times, :integer
   end
 end
